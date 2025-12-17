8e0c6371e4928df6f69a56baae4ff90fb2619ccb
DaShenZi <39253334+DaShenZi721@users.noreply.github.com>
2025-12-17 14:17:04 +0800

    Remove hardcoded model path in create_model function
    Removed hardcoded path for 'deberta-v3-base' model initialization.

--- nlu/DeBERTa/apps/run.py
+++ nlu/DeBERTa/apps/run.py
@@ -41,8 +41,6 @@ def create_model(args, num_labels, model_class_fn):
   # Prepare model
   rank = getattr(args, 'rank', 0)
   init_model = args.init_model if rank<1 else None
-  if init_model == 'deberta-v3-base':
-    init_model = '/home/HRA/OFT/oft/models/deberta-v3-base'
   model = model_class_fn(init_model, args.model_config, num_labels=num_labels, \
       drop_out=args.cls_drop_out, \
       pre_trained = args.pre_trained)
